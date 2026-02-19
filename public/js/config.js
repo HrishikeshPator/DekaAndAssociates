1868cb758ba37793c0dfb42a8461cbd114ec1290
HrishikeshPator <riisixx0@gmail.com>
2026-02-19 11:09:20 +0530

    fix: Un-ignore config.js and remove duplicate scripts to repair live CTA button

---
+++ public/js/config.js
@@ -0,0 +1,6 @@
+
+window.SUPABASE_CONFIG = {
+    url: 'https://oszhnktawzoizbtylqpo.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zemhua3Rhd3pvaXpidHlscXBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzNDM3ODIsImV4cCI6MjA4NjkxOTc4Mn0.fVmi4v0GlHdq86jnBwDX-O693xyyuBrQoT6yCnKI_nU',
+    adminEmails: ['dekaandassociate@gmail.com']
+};

586fbf6ff0ff8affa32265185039dc38fd6fdfeb
HrishikeshPator <riisixx0@gmail.com>
2026-02-22 00:53:05 +0530

    refactor: externalize and ignore Firebase config

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,18 +1,11 @@
+importScripts('/js/firebase-config.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
-firebase.initializeApp({
-  apiKey: "AIzaSyAAhyUKq3FjiUYJnYxXKWwnYLcoNGO1zaY",
-  authDomain: "dekaandassociatesghy.firebaseapp.com",
-  projectId: "dekaandassociatesghy",
-  storageBucket: "dekaandassociatesghy.firebasestorage.app",
-  messagingSenderId: "939359139918",
-  appId: "1:939359139918:web:e23e316fa07ec44d2e6b1f",
-  measurementId: "G-PD9SYHMDKK"
-});
+firebase.initializeApp(firebaseConfig);
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
